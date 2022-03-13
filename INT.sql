4d60bf243d02b920a985e5a1851065e775f353cc
asfawe <aronia5137>
2022-03-13 17:43:44 +0900

    first commit

---
+++ INT.sql
@@ -0,0 +1,5 @@
+CREATE TABLE hongon4 (
+	tinyint_col TINYINT,
+    samllint_col SMALLINT,
+    int_col INT, 
+    bigint_col BIGINT );
---
+++ select-1.sql
@@ -0,0 +1,2 @@
+USE market_db;
+SELECT height 키, debut_date "데뷔 일자", addr  FROM market_db.member;
